7d96249eca9e907c10945e404c218acb7e7f4676
wilton <wiltonlazary@gmail.com>
2018-06-23 01:35:54 -0300

    .

--- dumpast-draft_ml.sh
+++ dumpast-draft_ml.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ocamlfind ppx_tools/dumpast draft/draft_ml.ml | tail --lines=+3 | head --lines=-1 | refmt --print-width=140 --parse=ml > draft/draft_ml_ast.re
+ocamlfind ppx_tools/dumpast draft/draft_ml.ml | tail --lines=+3 | head --lines=-1
+#| refmt --print-width=140 --parse=ml > draft/draft_ml_ast.re
